466b14d6a11f1b941260da43b14f423f3d9a8b24
Wang Zhuochun <stone1551@gmail.com>
2013-06-15 15:34:10 +0800

    combine output with d3js

---
+++ js/dependency.js
@@ -0,0 +1,40 @@
+(function(app) {
+
+var moduleRegex = /[a-z]{2,3}\d{4}[a-z]{0,2}/ig;
+
+function tree(code) {
+    var node = {};
+
+    if (!app.raw[code]) {
+        //alert("module code [" + code + "] not found.");
+        return { name: code };
+    }
+
+    var mod = app.raw[code];
+
+    node.name = mod.code;
+    node.children = [];
+
+    var dep = mod.prerequisite.match( moduleRegex ) || [];
+
+    var i = 0, len = dep.length;
+
+    for (; i < len; i++) {
+        var child = tree(dep[i]);
+
+        child && node.children.push(child);
+    }
+
+    return node;
+}
+
+document.getElementById("submit")
+        .addEventListener("click", function() {
+            var code = document.getElementById("code").value;
+
+            var root = tree(code);
+
+            app.draw(root);
+        });
+
+})(window.app || {});
